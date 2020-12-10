3491502edc9d970aa745ec533eef950296f6dba5
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 21:39:00 +0200

    change

---
+++ C# OOP/DesignPatterns/TemplatePattern/TwelveGrain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplatePattern
+{
+    public class TwelveGrain : Bread
+    {
+        public TwelveGrain()
+        {
+        }
+
+        public override void MixIngridients()
+        {
+            Console.WriteLine("Gathering Ingridients for 12-Grain Bread.");     
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }  
+    }
+}
