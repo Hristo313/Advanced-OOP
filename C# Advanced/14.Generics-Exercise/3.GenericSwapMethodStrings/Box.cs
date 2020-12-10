d24fb9584917ba41230e4d550dce24ae10fd7261
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 20:10:34 +0200

    Generics - Exercise

---
+++ C# Advanced/14.Generics-Exercise/3.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.GenericSwapMethodStrings
+{
+	public class Box<T>
+	{
+		public Box(List<T> value)
+		{
+			this.Values = value;
+		}
+
+		public List<T> Values { get; set; }
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			foreach (var item in this.Values)
+			{
+				sb.AppendLine($"{item.GetType()}: {item} ");
+			}
+
+			return sb.ToString().TrimEnd();
+		}
+
+		public void Swap(List<T> items, int firstIndex, int secondIndex)
+		{
+			T temValue = items[firstIndex];
+			items[firstIndex] = items[secondIndex];
+			items[secondIndex] = temValue;
+		}
+	}
+}
---
+++ C# Advanced/14.Generics-Exercise/7.Tuple/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _7.Tuple
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			string[] person = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+			string fullName = person[0] + " " + person[1];
+			string adress = person[2];
+
+			string[] biersPerPerson = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+			string name = biersPerPerson[0];
+			int biers = int.Parse(biersPerPerson[1]);
+
+			string[] numbers = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+			int firstNumber = int.Parse(numbers[0]);
+			double secondNumber = double.Parse(numbers[1]);
+
+			var firstTuple = new MyTuple<string, string>(fullName, adress);
+			var secondTuple = new MyTuple<string, int>(name, biers);
+			var thirdTuple = new MyTuple<int, double>(firstNumber, secondNumber);
+
+			Console.WriteLine(firstTuple);
+			Console.WriteLine(secondTuple);
+			Console.WriteLine(thirdTuple);
+		}
+	}
+}
