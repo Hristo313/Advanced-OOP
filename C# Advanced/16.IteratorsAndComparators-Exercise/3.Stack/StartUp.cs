3de2e1329ed690d8ec4d28cc337a32815f0ede79
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 20:11:13 +0200

    Iterators And Comparators - Exercise

---
+++ C# Advanced/16.IteratorsAndComparators-Exercise/3.Stack/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.Stack
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			MyStack<int> myStack = new MyStack<int>();
+
+			while (true)
+			{
+				string[] line = Console.ReadLine()
+					.Split(new string[] {" ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+				string command = line[0];
+
+				if (command == "END")
+				{
+					break;
+				}
+
+				if (command == "Push")
+				{
+					foreach (var item in line.Skip(1))
+					{
+						myStack.Push(int.Parse(item));
+					}
+				}
+				else if (command == "Pop")
+				{
+					if(myStack.Count == 0)
+					{
+						Console.WriteLine("No elements");
+					}
+					else
+					{
+						myStack.Pop();
+					}
+				}
+			}
+
+			foreach (var item in myStack)
+			{
+				Console.WriteLine(item);
+			}
+
+			foreach (var item in myStack)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
