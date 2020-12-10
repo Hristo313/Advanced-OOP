4ca3cc475df600e3299920548664c11b8785cdfc
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 20:10:18 +0200

    Generics - Lab

---
+++ C# Advanced/13.Generics-Lab/1.BoxT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.Box
+{
+	public class Box<T>
+	{
+		private List<T> data;
+
+		public Box()
+		{
+			this.data = new List<T>();
+		}
+
+		public int Count
+		{
+			get
+			{
+				return this.data.Count;
+			}
+		}
+
+		public void Add(T element)
+		{
+			this.data.Add(element);
+		}
+
+		public T Remove()
+		{
+			var item = this.data.Last();
+			this.data.RemoveAt(this.data.Count - 1);
+			return item;
+		}	
+	}
+}
