92740e5c52678116c0074b651642bc648bb3e916
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 20:04:41 +0200

    Streams, Files And Directories - Lab

---
+++ C# Advanced/7.Streams,FilesAndDirectories-Lab/02.LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using var reader = new StreamReader("input.txt");
+			using var writer = new StreamWriter("output.txt");
+
+			int counter = 1;
+
+			while (true)
+			{
+				var line = reader.ReadLine();
+
+				if (line == null)
+				{
+					break;
+				}
+
+				writer.WriteLine($"{counter++}. {line}");
+			}
+		}
+	}
+}
---
+++ C# Advanced/7.Streams,FilesAndDirectories-Lab/03.WordCount/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{		
+			string wordToCount = File.ReadAllText("words.txt");
+			string wordInInput = File.ReadAllText("input.txt");			
+
+			string symbols = " ";
+
+			foreach (var @char in wordInInput)
+			{
+				if (Char.IsPunctuation(@char) && @char != '\'') // Take all punctoation symbols 
+				{
+					symbols += @char;
+				}
+			}
+
+			string[] allWordsToCount = wordToCount.ToLower().Split(symbols.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			string[] allWordsInInput = wordInInput.ToLower().Split(symbols.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			// Split to all punctoation symbols to take the free words
+
+			var words = new Dictionary<string, int>();
+
+			for (int i = 0; i < allWordsToCount.Length; i++)
+			{
+				string currentWordToCount = allWordsToCount[i];
+
+				if (!words.ContainsKey(currentWordToCount))
+				{
+					words[currentWordToCount] = 0;
+				}
+
+				for (int j = 0; j < allWordsInInput.Length; j++)
+				{
+					string currentWordInInput = allWordsInInput[j];
+
+					if(currentWordToCount == currentWordInInput)
+					{
+						words[currentWordToCount]++;
+					}
+				}
+			}
+
+			using var writer = new StreamWriter("../../../output.txt");
+
+			foreach (var kvp in words.OrderByDescending(x => x.Value))
+			{
+				string word = kvp.Key;
+				int count = kvp.Value;
+				writer.WriteLine($"{word} - {count}");		
+			}
+		}
+	}
+}
