d7ace6112cf8426c1327a5c5c40e082ff4be930f
Hristo313 <insomnia_2000@abv.bg>
2020-12-10 20:06:46 +0200

    Functional Programming - Lab

---
+++ C# Advanced/9.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine()
+				.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.Where(x => x % 2 == 0)
+				.OrderBy(x => x)
+				.ToArray();
+
+			string result = string.Join(", ", numbers);
+			Console.WriteLine(result);
+		}
+	}
+}
---
+++ C# Advanced/9.FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine()
+				.Split()
+				.Where(word => char.IsUpper(word[0]))
+				.ToList()
+				.ForEach(Console.WriteLine);
+		}
+	}
+}
