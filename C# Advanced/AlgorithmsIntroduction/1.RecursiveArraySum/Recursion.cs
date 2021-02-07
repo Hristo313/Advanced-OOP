15198ab4e461ebb4ab743b08830fc5a99dd0e915
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-07 12:30:00 +0200

    Add Algorithms

---
+++ C# Advanced/AlgorithmsIntroduction/1.RecursiveArraySum/Recursion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.RecursiveArraySum
+{
+	public class Recursion
+	{
+		public static void Main(string[] args)
+		{
+			int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			Console.WriteLine(Sum(arr, 0));
+
+			int n = int.Parse(Console.ReadLine());
+			Console.WriteLine(Factorial(n));
+		}
+
+		public static int Factorial(int n)
+		{
+			if(n == 1)
+			{
+				return 1;
+			}
+
+			return n * Factorial(n - 1);
+		}
+
+		public static int Sum(int[] arr, int index)
+		{
+			if(index == arr.Length - 1)
+			{
+				return arr[index];
+			}
+
+			return arr[index] + Sum(arr, index + 1);
+		}
+
+		public static int Pow(int x, int n)
+		{
+			if (n == 1)
+			{
+				return x;
+			}
+
+			return x * Pow(x, n - 1);
+		}
+	}
+}
---
+++ C# Advanced/AlgorithmsIntroduction/2.SumofCoins/GreedyAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.SumofCoins
+{
+	public class GreedyAlgorithm
+	{
+		public static void Main(string[] args)
+		{
+			var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+			var targetSum = 923;
+
+			var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+			Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+			foreach (var selectedCoin in selectedCoins)
+			{
+				Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+			}
+		}
+
+		public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+		{
+			var result = new Dictionary<int, int>();
+			var sortedCoins = coins.OrderByDescending(c => c).ToList();
+
+			for (int currentCoinIndex = 0; currentCoinIndex < sortedCoins.Count; currentCoinIndex++)
+			{
+				var currentCoin = sortedCoins[currentCoinIndex];
+				var totalCurrentCoins = targetSum / currentCoin;
+				targetSum %= currentCoin;
+
+				result[currentCoin] = totalCurrentCoins;
+			}
+
+			return result;
+		}
+	}
+}
