8eb9e006920830d82403cc3f19f9362654ff6ac8
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-08 19:56:45 +0200

    Add Iterators

---
+++ C# Advanced/8.IteratorsAndComparators-Exercise/3.Stack/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.Stack
+{
+	public class MyStack<T> : IEnumerable<T>
+	{
+		private List<T> items;
+
+		public MyStack()
+		{
+			items = new List<T>();
+		}
+
+		public int Count => this.items.Count;
+
+		public void Push(T item)
+		{
+			this.items.Add(item);
+		}
+
+		public T Pop()
+		{
+			T item = this.items[this.items.Count - 1]; //[^1]
+			this.items.RemoveAt(this.items.Count - 1);
+			return item;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = this.items.Count - 1; i >= 0; i--)
+			{
+				yield return this.items[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+	}
+}
