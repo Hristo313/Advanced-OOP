901c3c7442425c430636bf287f7bce2616a9e5a7
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-08 19:54:36 +0200

    Add Stack And Queue

---
+++ C# Advanced/1.StackAndQueues-Exercise/03.MaximumAndMinimumProblem/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumProblem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			Stack<int> numbers = new Stack<int>();
+
+			for (int i = 0; i < n; i++)
+			{
+				string[] arguments = Console.ReadLine()
+					.Split()
+					.ToArray();
+
+				string type = arguments[0];
+
+				if (type == "1")
+				{
+					int element = int.Parse(arguments[1]);
+					numbers.Push(element);
+				}
+				else if (type == "2")
+				{
+					if (numbers.Any())
+					{
+						numbers.Pop();
+					}
+				}
+				else if (type == "3")
+				{
+					if (numbers.Any())
+					{
+						Console.WriteLine(numbers.Max());
+					}
+				}
+				else if (type == "4")
+				{
+					if (numbers.Any())
+					{
+						Console.WriteLine(numbers.Min());
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join(", ", numbers));
+		}
+	}
+}
