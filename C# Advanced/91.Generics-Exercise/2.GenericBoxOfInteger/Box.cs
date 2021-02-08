3eaa6d2977d55936e58eb81c0ddb16293b3aa77f
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-08 20:16:13 +0200

    Updates

---
+++ C# Advanced/91.Generics-Exercise/2.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2.GenericBoxOfInteger
+{
+	public class Box<T>
+	{
+		public Box(T value)
+		{
+			this.Value = value;
+		}
+
+		public T Value { get; set; }
+
+		public override string ToString()
+		{
+			return $"{this.Value.GetType()}: {this.Value}";
+		}
+	}
+}
---
+++ C# Advanced/91.Generics-Exercise/2.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _2.GenericBoxOfInteger
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int numberOfStrings = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < numberOfStrings; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+
+				Box<int> box = new Box<int>(number);
+				Console.WriteLine(box);
+			}
+		}
+	}
+}
