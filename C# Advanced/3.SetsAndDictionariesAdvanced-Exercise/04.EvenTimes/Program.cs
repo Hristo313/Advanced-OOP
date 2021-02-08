3c65c966c299c7c0eb8d467594af2301ad4854c1
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-08 19:55:25 +0200

    Add Sets and Dictionaries

---
+++ C# Advanced/3.SetsAndDictionariesAdvanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int countOfIntegers = int.Parse(Console.ReadLine());
+
+			var numbers = new Dictionary<int, int>();
+
+			for (int i = 0; i < countOfIntegers; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+
+				if (!numbers.ContainsKey(number))
+				{
+					numbers[number] = 0;
+				}
+
+				numbers[number]++;
+			}
+
+			foreach (var kvp in numbers)
+			{
+				int number = kvp.Key;
+				int times = kvp.Value;
+
+				if (times % 2 == 0)
+				{
+					Console.Write(number);
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ C# Advanced/3.SetsAndDictionariesAdvanced-Exercise/06.Wardrobe/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Wardrobe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numberOfClothes = int.Parse(Console.ReadLine());
+
+			var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+			for (int i = 0; i < numberOfClothes; i++)
+			{
+				string[] line = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+				string color = line[0];
+				string[] clothes = line[1].Split(",");
+
+				if (!wardrobe.ContainsKey(color))
+				{
+					wardrobe[color] = new Dictionary<string, int>();
+				}
+
+				for (int j = 0; j < clothes.Length; j++)
+				{
+					string clothe = clothes[j];
+
+					if (!wardrobe[color].ContainsKey(clothe))
+					{
+						wardrobe[color][clothe] = 0;
+					}
+
+					wardrobe[color][clothe]++;
+				}
+			}
+
+			string[] searchingItem = Console.ReadLine().Split();
+			string searchingColor = searchingItem[0];
+			string searchingClothe = searchingItem[1];
+
+			bool itemIsFound = false;
+
+			foreach (var kvp in wardrobe)
+			{
+				string color = kvp.Key;
+
+				if(color == searchingColor)
+				{
+					itemIsFound = true;
+				}
+
+				var clothes = kvp.Value;
+
+				Console.WriteLine($"{color} clothes:");
+				foreach (var kvpClothe in clothes)
+				{
+					string clothe = kvpClothe.Key;
+					int times = kvpClothe.Value;
+
+					if(itemIsFound && clothe == searchingItem[1])
+					{
+						Console.WriteLine($"* {clothe} - {times} (found!)");
+					}
+					else
+					{
+						Console.WriteLine($"* {clothe} - {times} ");
+					}					
+				}
+			}
+		}
+	}
+}
