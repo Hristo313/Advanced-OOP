f2fb35db887850e1c99eabb53f1339e2e92cc3a2
Hristo Hristov <insomnia_2000@abv.bg>
2021-02-08 19:56:27 +0200

    Add Generics

---
+++ C# Advanced/7.Generics-Exercise/1.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1.GenericBoxOfString
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int numberOfStrings = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < numberOfStrings; i++)
+			{
+				string text = Console.ReadLine();
+
+				Box<string> box = new Box<string>(text);
+				Console.WriteLine(box);
+			}			
+		}
+	}
+}
---
+++ C# Advanced/7.Generics-Exercise/5.GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.GenericCountMethodStrings
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int numberOfStrings = int.Parse(Console.ReadLine());
+			List<string> elements = new List<string>();
+
+			for (int i = 0; i < numberOfStrings; i++)
+			{
+				string element = Console.ReadLine();
+				elements.Add(element);
+			}
+
+			Box<string> box = new Box<string>(elements);
+
+			string elementToCompare = Console.ReadLine();
+
+			int result = box.CompareElements(elements, elementToCompare);
+			Console.WriteLine(result);
+		}
+	}
+}
